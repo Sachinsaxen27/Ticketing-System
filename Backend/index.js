e91ca8a16ca2326eab00f83e556341a146a45dd2
Sachin Saxena <saxenasachin186@gmail.com>
2025-05-01 18:25:31 +0530

    Cors change

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,25 @@
 require('dotenv').config()
 
-const express=require('express')
-const database=require('./Database/Database')
-const cors=require('cors')
+const express = require('express')
+const database = require('./Database/Database')
+const cors = require('cors')
 database()
 
-const app=express()
-const port= process.env.PORT || 5000;
-app.use(cors({ origin: 'https://magenta-begonia-83cf73.netlify.app', credentials: true }))
-app.options("*",cors({origin: 'https://magenta-begonia-83cf73.netlify.app', credentials: true}))
-app.use(express.json({limit:"10mb",extended:true}))
-app.use(express.urlencoded({limit:"10mb",extended:true,parameterLimit:50000}))
+const app = express()
+const port = process.env.PORT || 5000;
+const corsvalue = {
+    origin: 'https://magenta-begonia-83cf73.netlify.app',
+    credentials: true
+}
+app.use(cors(corsvalue))
+app.options("*", cors(corsvalue))
+app.use(express.json({ limit: "10mb", extended: true }))
+app.use(express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 }))
 
-app.use('/api/adminlogin',require('./Data/AdminData'))
-app.use('/api/memberlogin',require('./Data/MemberData'))
-app.use('/api/userlogin',require('./Data/UserData'))
-app.use('/api/messagebox',require('./Data/MessageData'))
-app.listen(port,()=>{
+app.use('/api/adminlogin', require('./Data/AdminData'))
+app.use('/api/memberlogin', require('./Data/MemberData'))
+app.use('/api/userlogin', require('./Data/UserData'))
+app.use('/api/messagebox', require('./Data/MessageData'))
+app.listen(port, () => {
     console.log(`Ticket System is Online at http://localhost:${port}`)
 })
