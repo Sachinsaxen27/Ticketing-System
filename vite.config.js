58015ce82e251dadebae8fcc5d7121e5bae3e863
Sachin Saxena <saxenasachin186@gmail.com>
2025-05-02 12:35:12 +0530

    _redirects

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: '.' // <- puts it in dist/
+        }
+      ]
+    })
+  ],
 })
